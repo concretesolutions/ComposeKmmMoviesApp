0a02a667f19ace0e02c66114f07ced66a1f7ce29
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-03-09 14:37:07 -0300

    Add scaffold screen to build app structure

--- androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/MainActivity.kt
+++ androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/MainActivity.kt
@@ -3,18 +3,12 @@ package br.com.concrete.composekmmmoviesapp.androidApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import br.com.concrete.composekmmmoviesapp.shared.Greeting
-
-fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(greet())
+            MoviesApp()
         }
     }
 }
---
+++ androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/model/MoviesAppScreen.kt
@@ -0,0 +1,23 @@
+package br.com.concrete.composekmmmoviesapp.androidApp.model
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Movie
+import androidx.compose.ui.graphics.vector.ImageVector
+import br.com.concrete.composekmmmoviesapp.androidApp.R
+
+sealed class Screen(
+    val route: String,
+    @StringRes val titleRes: Int,
+    val icon: ImageVector = Icons.Default.Movie
+) {
+    object Movies : Screen("movies", R.string.title_tab_movies, Icons.Default.Movie)
+    object Favorites : Screen("favorites", R.string.title_tab_favorites, Icons.Default.Favorite)
+    object Detail : Screen("detail", R.string.title_tab_favorites)
+}
+
+val bottomBarItems = listOf(
+    Screen.Movies,
+    Screen.Favorites,
+)
