f68642cc17f8e32489bb8829df8d6203f12f2f8d
Camila Fonseca <camila.fonseca.silva@accenture.com>
2021-03-09 10:04:05 -0300

    Adiciona as Libs do KMM

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,20 @@
 buildscript {
+
+    val kotlinVersion = "1.4.20"
+    val sqlDelightVersion = "1.4.4"
+
     repositories {
         gradlePluginPortal()
         jcenter()
         google()
         mavenCentral()
     }
+
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
     }
 }
 
