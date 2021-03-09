e792cff3b26d32eed764e79f98693a7fb8da0cfb
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-03-09 09:11:11 -0300

    Increase memory for Gradle

--- androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/MainActivity.kt
+++ androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/MainActivity.kt
@@ -1,20 +1,20 @@
 package br.com.concrete.composekmmmoviesapp.androidApp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 import br.com.concrete.composekmmmoviesapp.shared.Greeting
-import android.widget.TextView
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            Text(greet())
+        }
     }
 }
