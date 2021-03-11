fdfef691b0b6144eda9eda45becab6b0c3684738
Shayanne Amorim <shayanneirlanda@gmail.com>
2021-03-11 14:54:07 -0300

    Lista de Favoritos em andamento

---
+++ androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/favoriteslist/DataList.kt
@@ -0,0 +1,9 @@
+package br.com.concrete.composekmmmoviesapp.androidApp.favoriteslist
+
+class GapList (
+    var name: String,
+    var year: String,
+    var gender: String,
+    var description: String,
+    var imageUrl: Int
+        )
---
+++ androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/ui/theme/Colors.kt
@@ -0,0 +1,2 @@
+package br.com.concrete.composekmmmoviesapp.androidApp.ui.theme
+
