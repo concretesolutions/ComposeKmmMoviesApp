6037aeb9a7ef60a0f1905444aea16fe8108ca0bd
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-03-11 15:33:08 -0300

    Adjust layout color

--- androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/popularMovie/MovieCard.kt
+++ androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/popularMovie/MovieCard.kt
@@ -2,18 +2,18 @@ package br.com.concrete.composekmmmoviesapp.androidApp.popularMovie
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -31,11 +31,11 @@ fun MovieListView(
     list: List<MovieDao>
 ) {
     LazyVerticalGrid(cells = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp),
         content = {
             items(list) { movie ->
                 MovieItemView(movieDao = movie)
             }
-
         })
 
 
@@ -44,42 +44,36 @@ fun MovieListView(
 @Composable
 fun MovieItemView(movieDao: MovieDao) {
     Card(
-        modifier = Modifier
-            .padding(8.dp)
+        backgroundColor = Color.Gray,
+        modifier = Modifier.padding(8.dp)
     ) {
-        Surface(
-            modifier = Modifier
-                .padding(9.dp)
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp)
         ) {
-            Column {
-                val image: Painter = painterResource(movieDao.moviePicture)
-                Image(
-                    painter = image, contentDescription = "",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.clip(RoundedCornerShape(8.dp))
+            val image: Painter = painterResource(movieDao.moviePicture)
+            Image(
+                painter = image, contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .aspectRatio(9f / 16)
+            )
+            Row {
+                Text(
+                    text = movieDao.name,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(start = 8.dp, top = 16.dp)
+                        .weight(1f)
                 )
-                Row {
-                    Text(
-                        text = movieDao.name,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.padding(start = 8.dp, top = 16.dp)
-
-
-                    )
-                    Spacer(modifier = Modifier.padding(45.dp,0.dp))
-                    IconButton(onClick = {  /*doSomething()*/ }) {
-                        Icon(Icons.Filled.Favorite, contentDescription = null)
-
-                    }
+                IconButton(onClick = {  /*doSomething()*/ }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
                 }
-
             }
-
         }
-
     }
 }
-
 
 
 @ExperimentalFoundationApi
