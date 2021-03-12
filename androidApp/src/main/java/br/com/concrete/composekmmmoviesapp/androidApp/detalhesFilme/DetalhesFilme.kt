d4e8d832dc81e196e4381200f385e0dd7cef5c53
cs-rentakelly <renakelly@gmail.com>
2021-03-12 09:46:12 -0300

    Adiciona icone favorito

--- androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/detalhesFilme/DetalhesFilme.kt
+++ androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/detalhesFilme/DetalhesFilme.kt
@@ -8,13 +8,19 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,8 +32,8 @@ class DetalhesFilme : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NewsStory(
-                R.drawable.filme,
-                filme = "Se Enlouquecer, Não se Apaixone",
+                R.drawable.olga,
+                filme = "Olga",
                 ano = "2010",
                 genero = "Drama, Romance",
                 sinopse = "Craig (Keir Gilchrist), estressado com as demandas de" +
@@ -59,9 +65,9 @@ fun NewsStory(
                 .verticalScroll(rememberScrollState())
         ) {
             val imageModifier = Modifier
-                .aspectRatio(1f)
                 .fillMaxWidth()
-                .clip(shape = RoundedCornerShape(4.dp))
+                .aspectRatio(1f)
+                .clip(shape = RoundedCornerShape(8.dp))
 
             Image(
                 image,
@@ -69,13 +75,29 @@ fun NewsStory(
                 contentDescription = "",
                 contentScale = ContentScale.Crop
             )
-            Spacer(Modifier.height(16.dp))
+            Spacer(Modifier.height(8.dp))
 
+            Row() {
+                Text(
+                    filme,
+                    style = typography.h4,
+                    maxLines = 2,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(
+                    modifier = Modifier
+                        .padding(120.dp, 0.dp, 0.dp, 0.dp)
+                        .weight(1f)
+                )
+                IconButton(
+                    onClick = { }) {
+                    Icon(
+                        Icons.Filled.Favorite,
+                        contentDescription = null
+                    )
+                }
+            }
 
-            Text(
-                filme,
-                style = typography.h6, maxLines = 2
-            )
             Text(
                 "Ano",
                 style = typography.h6
@@ -108,8 +130,8 @@ fun NewsStory(
 @Composable
 fun DefultPreview() {
     NewsStory(
-        R.drawable.filme,
-        filme = "Se Enlouquecer, Não se Apaixone",
+        R.drawable.olga,
+        filme = "Olga",
         ano = "2010",
         genero = "Drama, Romance",
         sinopse = "Craig (Keir Gilchrist), estressado com as demandas de" +
