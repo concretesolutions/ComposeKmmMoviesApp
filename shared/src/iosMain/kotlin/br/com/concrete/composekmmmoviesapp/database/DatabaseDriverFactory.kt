266de6d7fe3e71d16a56d536d6b46c3aa9dab88a
cs-lucas-costa <lucas.henrique.costa@concrete.com.br>
2021-03-16 15:02:09 -0300

    Import NativeSqliteDriver iOS

--- shared/src/iosMain/kotlin/br/com/concrete/composekmmmoviesapp/database/DatabaseDriverFactory.kt
+++ shared/src/iosMain/kotlin/br/com/concrete/composekmmmoviesapp/database/DatabaseDriverFactory.kt
@@ -2,6 +2,7 @@ package br.com.concrete.composekmmmoviesapp.database
 
 import br.com.concrete.composekmmmoviesapp.cache.AppDatabase
 import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
 actual class DatabaseDriverFactory {
     actual fun createDriver(): SqlDriver {
