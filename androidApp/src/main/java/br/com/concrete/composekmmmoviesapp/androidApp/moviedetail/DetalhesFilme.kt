6f241a9cf9d66dae79bd8f484639266b8cd4a138
cs-rentakelly <renakelly@gmail.com>
2021-03-16 11:38:43 -0300

    Adiciona e centraliza o botão favorito

--- androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/moviedetail/DetalhesFilme.kt
+++ androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/moviedetail/DetalhesFilme.kt
@@ -5,19 +5,23 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.concrete.composekmmmoviesapp.androidApp.common.FavoriteMovieButton
 import br.com.concrete.composekmmmoviesapp.androidApp.data.model.Movie
 import br.com.concrete.composekmmmoviesapp.androidApp.util.DataSamples
 import dev.chrisbanes.accompanist.coil.CoilImage
 
 @Composable
-fun MovieDetailScreen(movie: Movie) {
+fun MovieDetailScreen(movie: Movie, onClickFavorite: (Movie) -> Unit) {
     MaterialTheme {
         val typography = MaterialTheme.typography
         Column(
@@ -39,10 +43,26 @@ fun MovieDetailScreen(movie: Movie) {
             Spacer(Modifier.height(16.dp))
 
 
-            Text(
-                text = movie.title,
-                style = typography.h6, maxLines = 2
-            )
+            Row() {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(0.85f)
+                        .wrapContentWidth(Alignment.Start)
+                        .align(Alignment.CenterVertically),
+                    text = movie.title,
+                    style = typography.h4,
+                    fontWeight = FontWeight.Bold
+                )
+                Surface(
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .wrapContentWidth(Alignment.End)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    FavoriteMovieButton(movie, onClickFavorite)
+                }
+
+            }
             Text(
                 "Ano",
                 style = typography.h6
@@ -74,5 +94,5 @@ fun MovieDetailScreen(movie: Movie) {
 @Preview
 @Composable
 fun DefultPreview() {
-    MovieDetailScreen(DataSamples.movie)
+    MovieDetailScreen(DataSamples.movie, {})
 }
