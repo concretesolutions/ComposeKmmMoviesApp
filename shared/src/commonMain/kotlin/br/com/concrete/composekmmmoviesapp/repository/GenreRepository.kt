67648f15fe6c266213d65262c0d63949e5b2a08e
Luiz Fernando <luiz.silva@concrete.com.br>
2021-03-16 11:35:18 -0300

    Adding GenreDao field to GenreRepository

--- shared/src/commonMain/kotlin/br/com/concrete/composekmmmoviesapp/repository/GenreRepository.kt
+++ shared/src/commonMain/kotlin/br/com/concrete/composekmmmoviesapp/repository/GenreRepository.kt
@@ -1,10 +1,11 @@
 package br.com.concrete.composekmmmoviesapp.repository
 
 import br.com.concrete.composekmmmoviesapp.data.Response
+import br.com.concrete.composekmmmoviesapp.database.GenreDao
 import br.com.concrete.composekmmmoviesapp.domain.Genres
 import br.com.concrete.composekmmmoviesapp.network.GenreApi
 
-class GenreRepository (private val genreApi : GenreApi) {
+class GenreRepository (private val genreApi : GenreApi, private val genreDao: GenreDao) {
 
     suspend fun getGenresList(): Response<Genres> = genreApi.getGenres()
 
