82c10882e37ed8f09a19dcac2831c0c7889ad3f8
cs-rodrigofogaca <rodrigo.fogaca@accenture.com>
2021-03-26 16:22:39 -0300

    remove comments

--- androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/data/mapper/MoviesMapper.kt
+++ androidApp/src/main/java/br/com/concrete/composekmmmoviesapp/androidApp/data/mapper/MoviesMapper.kt
@@ -8,36 +8,6 @@ class MoviesMapper {
         const val imageBaseUrl = "https://image.tmdb.org/t/p/w500/"
     }
 
-//    fun mapMoviesDtoToMovie(
-//        moviePage: MoviePageDto,
-//        favoriteMovies: List<MovieResponse>,
-//        genreDto: GenreDto
-//    ): List<MovieResponse> {
-//        return moviePage.movieItems.map { apiMovie ->
-//            val movieGenres = apiMovie.genreIds.map { genreId ->
-//                val genreApiDto = genreDto.genres.find { genreDto ->
-//                    genreDto.id == genreId
-//                }
-//
-//                if (genreApiDto != null) {
-//                    Genre(genreApiDto.id, genreApiDto.name)
-//                } else {
-//                    null
-//                }
-//            }.filterNotNull()
-//
-//            MovieResponse(
-//                id = apiMovie.id,
-//                title = apiMovie.title,
-//                imageUrl = "${imageBaseUrl}${apiMovie.backdropPath}",
-//                genres = movieGenres,
-//                overview = apiMovie.overview,
-//                releaseYear = apiMovie.releaseDate.parseToDate()?.getYearFromDate() ?: -1,
-//                isfavorite = favoriteMovies.any { it.id == apiMovie.id }
-//            )
-//        }
-//    }
-
     fun mapMovieToMoviesDTO(moviesSdk: List<br.com.concrete.composekmmmoviesapp.domain.Movie>): List<Movie> {
         return moviesSdk.map {
             Movie(
